068c3f9a42110f6e735d4a49d6d9c3be75dac251
ramjetsq-ui <ramjetsq@gmail.com>
2025-08-21 21:29:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   tokens: "8024991362:AAGlvsMPIP_MUEhf9lqcPDYyINu_jFhVrvM", 
-  owner: "5239114164", 
+  owner: "5126860596", 
   port: "3000",
   ipvps: "178.128.80.134" // ubah jadi vps mu !!!
 };
